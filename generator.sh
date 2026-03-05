08950a9133740fb80fdd5a3f54f84fd52de4e057
DeviceArtist <deviceartist@sohu.com>
2026-03-05 15:16:05 +0800

    Update generator.sh

--- generator.sh
+++ generator.sh
@@ -1,5 +1,3 @@
 source .env/bin/activate
-export HF_HOME=./cache/hf_cache
-export TRANSFORMERS_CACHE=./cache/transformers_cache
-export HF_ENDPOINT=https://xxx
+source ./config.sh
 uv run image_generator.py --output "$1" --prompt "$2"
